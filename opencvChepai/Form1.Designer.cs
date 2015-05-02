ffe6dd49896d7658d8747fc28df6b08f59b3f066
ypw <915505626@qq.com>
2015-05-02 19:44:44 +0800

    master

---
+++ opencvChepai/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace opencvChepai
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(13, 44);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(254, 258);
+            this.imageBox1.TabIndex = 1;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(13, 308);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(255, 120);
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(276, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(476, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(177, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 662);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox imageBox2;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ opencvChepai/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.Util;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+
+
+namespace opencvChepai
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String[] ports = SerialPort.GetPortNames();
+            if (ports.Length > 0)
+            {
+                serialPort1.PortName = ports[0];
+                serialPort1.Open();
+                this.Text = "open";
+            }
+        }
+        Image<Bgr, Byte> img;
+        int a;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //CvInvoke.cvCreateCameraCapture(2);
+            Capture capture = new Capture();
+            Application.Idle += new EventHandler(delegate(object sender2, EventArgs e2)
+            {
+                img = capture.QueryFrame();
+                imageBox1.Image = img.ToBitmap();
+                if (a > 0)
+                {
+                    getBox(img);
+                    a = 0;
+                }
+            });
+        }
+
+        public void getBox(Image<Bgr, Byte> img)
+        {
+            Image<Bgr, Byte> simage = img;    //new Image<Bgr, byte>("license-plate.jpg");
+            //Image<Bgr, Byte> simage = sizeimage.Resize(400, 300, Emgu.CV.CvEnum.INTER.CV_INTER_NN);
+            Image<Gray, Byte> GrayImg = new Image<Gray, Byte>(simage.Width, simage.Height);
+            IntPtr GrayImg1 = CvInvoke.cvCreateImage(simage.Size, Emgu.CV.CvEnum.IPL_DEPTH.IPL_DEPTH_8U, 1);
+            //灰度化
+            CvInvoke.cvCvtColor(simage.Ptr, GrayImg1, Emgu.CV.CvEnum.COLOR_CONVERSION.BGR2GRAY);
+            //首先创建一张16深度有符号的图像区域
+            IntPtr Sobel = CvInvoke.cvCreateImage(simage.Size, Emgu.CV.CvEnum.IPL_DEPTH.IPL_DEPTH_16S, 1);
+            //X方向的Sobel算子检测
+            CvInvoke.cvSobel(GrayImg1, Sobel, 2, 0, 3);
+            IntPtr temp = CvInvoke.cvCreateImage(simage.Size, Emgu.CV.CvEnum.IPL_DEPTH.IPL_DEPTH_8U, 1);
+            CvInvoke.cvConvertScale(Sobel, temp, 0.00390625, 0);
+            ////int it = ComputeThresholdValue(GrayImg.ToBitmap());
+            ////二值化处理
+            ////Image<Gray, Byte> dest = GrayImg.ThresholdBinary(new Gray(it), new Gray(255));
+            Image<Gray, Byte> dest = new Image<Gray, Byte>(simage.Width, simage.Height);
+            //二值化处理
+            CvInvoke.cvThreshold(temp, dest, 0, 255, Emgu.CV.CvEnum.THRESH.CV_THRESH_OTSU);
+            IntPtr temp1 = CvInvoke.cvCreateImage(simage.Size, Emgu.CV.CvEnum.IPL_DEPTH.IPL_DEPTH_8U, 1);
+            Image<Gray, Byte> dest1 = new Image<Gray, Byte>(simage.Width, simage.Height);
+            CvInvoke.cvCreateStructuringElementEx(3, 1, 1, 0, Emgu.CV.CvEnum.CV_ELEMENT_SHAPE.CV_SHAPE_RECT, temp1);
+            CvInvoke.cvDilate(dest, dest1, temp1, 6);
+            CvInvoke.cvErode(dest1, dest1, temp1, 7);
+            CvInvoke.cvDilate(dest1, dest1, temp1, 1);
+            CvInvoke.cvCreateStructuringElementEx(1, 3, 0, 1, Emgu.CV.CvEnum.CV_ELEMENT_SHAPE.CV_SHAPE_RECT, temp1);
+            CvInvoke.cvErode(dest1, dest1, temp1, 2);
+            CvInvoke.cvDilate(dest1, dest1, temp1, 2);
+            IntPtr dst = CvInvoke.cvCreateImage(simage.Size, Emgu.CV.CvEnum.IPL_DEPTH.IPL_DEPTH_8U, 3);
+            CvInvoke.cvZero(dst);
+            //dest.Dilate(10);
+            //dest.Erode(5);
+            using (MemStorage stor = new MemStorage())
+            {
+                Contour<Point> contours = dest1.FindContours(
+                    Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE,
+                    Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_CCOMP,
+                    stor);
+                for (; contours != null; contours = contours.HNext)
+                {
+
+                    Rectangle box = contours.BoundingRectangle;
+                    
+                    Image<Bgr, Byte> test = simage.CopyBlank();
+                    test.SetValue(255.0);
+                    double whRatio = (double)box.Width / box.Height;
+                    int area = (int)box.Width * box.Height;
+                    if (area > 1000 && area < 10000)
+                    {
+                        if ((3.0 < whRatio && whRatio < 6.0))
+                        {
+                            test.Draw(box, new Bgr(Color.Red), 2);
+                            simage.Draw(box, new Bgr(Color.Red), 2);//CvInvoke.cvNamedWindow("dst");
+                            //CvInvoke.cvShowImage("dst", dst);
+                            //box.X += 6;
+                            //box.Width -= 12;
+                            Image<Gray, Byte> img2 = cut(simage, box);
+                            imageBox2.Image = img2.ToBitmap();
+                        }
+                    }
+                }
+            }
+        }
+
+        public Image<Gray, Byte> cut(Image<Bgr, byte> image, Rectangle rectangle)
+        {
+            var frame = image.Convert<Gray, Byte>();
+            frame.ROI = rectangle;
+            CvInvoke.cvThreshold(frame, frame, 0, 255, Emgu.CV.CvEnum.THRESH.CV_THRESH_OTSU);
+            
+            Byte[, ,] data = frame.Data;
+            Byte[] hist = new Byte[rectangle.Width];
+            Image<Bgr, Byte> imageHist = new Image<Bgr, Byte>(rectangle.Width, 255, new Bgr(255d, 255d, 255d));
+            Bgr black = new Bgr(0d, 0d, 0d);
+            for (int i = 0; i < rectangle.Width; i++)
+            {
+                int s = 0;
+                for (int j = 0; j < rectangle.Height; j++)
+                {
+                    if (data[j+rectangle.Y, i + rectangle.X, 0] > 120) s++;
+                }
+                if(s>5)hist[i] = (Byte)(s);
+                
+            }
+
+            for (int i = 0; i < rectangle.Width; i++)
+            {
+                LineSegment2D line = new LineSegment2D(new Point(i, 255), new Point(i, 255 - hist[i]));
+                imageHist.Draw(line, black, 1);
+            }
+            imageBox1.Image = imageHist.ToBitmap();
+            return frame;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a = 1;
+            Image<Bgr, Byte> img = new Image<Bgr, byte>("1.jpg");
+            imageBox1.Image = img.ToBitmap();
+            getBox(img);
+
+            if (serialPort1.IsOpen)
+            {
+                byte[] data1 = {0xDA, 0xAD, 0x88, 0x27};
+                byte[] data2 = {0xDB, 0xBD};
+                String asd = "粤B OK999 ";
+                System.Text.Encoding utf8, gb2312;
+                utf8 = System.Text.Encoding.GetEncoding("utf-8");
+                gb2312 = System.Text.Encoding.GetEncoding("gb2312");
+                byte[] chepai = System.Text.Encoding.Convert(utf8, gb2312, utf8.GetBytes(asd));
+
+                byte[] send = new byte[data1.Length + data2.Length + chepai.Length];
+                for (int i = 0; i < data1.Length; i++) send[i] = data1[i];
+                for (int i = 0; i < chepai.Length; i++) send[i+data1.Length] = chepai[i];
+                for (int i = 0; i < data2.Length; i++) send[i + chepai.Length+data1.Length] = data2[i];
+                for (int i = 0; i < send.Length; i++) textBox1.Text += Convert.ToString(send[i], 16).ToUpper() + " ";
+                serialPort1.Write(send, 0, send.Length);
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, Byte> img = new Image<Bgr, byte>("4.jpg");
+            imageBox1.Image = img.ToBitmap();
+            getBox(img);
+        }
+
+    }
+}
